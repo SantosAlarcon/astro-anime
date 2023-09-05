77a84b558b5b52ec71b52d8e63b211ddb97354f4
Santos Alarcón <santosalarcon86@gmail.com>
2023-09-05 21:03:14 +0200

    Fix build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
     redirects: {
         '/old-page': '/new-page'
     }
