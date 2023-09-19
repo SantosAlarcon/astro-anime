42e820af6cb6634a55b9cd7514c03c2b5621d961
Santos Alarcón <santosalarcon86@gmail.com>
2023-09-19 18:54:12 +0200

    Testing anime list width

--- src/scripts/nav-menu.js
+++ src/scripts/nav-menu.js
@@ -1,5 +1,4 @@
 const mobileBtn = document.querySelector(".mobile-nav-menu");
-console.log(mobileBtn);
 mobileBtn.addEventListener("click", () => {
     mobileBtn.classList.toggle("active");
     document.querySelector("body").classList.toggle("no-scroll");
