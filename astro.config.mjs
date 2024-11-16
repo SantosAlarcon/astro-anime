5ba2f35aec48cbb58b1c479d6ee5f40e5b800b05
Santos Alarcón <santosalarcon86@gmail.com>
2024-11-16 12:27:36 +0100

    Update dependencies and fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import {defineConfig} from "astro/config";
-import netlify from "@astrojs/netlify/functions"
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
+    integrations: [icon()],
     adapter: netlify(),
     redirects: {
-        '/old-page': '/new-page'
+        "/old-page": "/new-page",
     },
 });
